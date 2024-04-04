f2a68cc367dbab095cda52fd34fae0ae280fe4d0
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2024-04-04 12:22:50 +0200

    Commit acabar la part primcipal

--- js/mostrar.js
+++ js/mostrar.js
@@ -1,89 +1,89 @@
-let laboral = document.getElementById('laboral');
-let recursos = document.getElementById('recursos');
-let fiscal = document.getElementById('fiscal');
-let comptabilitat = document.getElementById('comptabilitat');
-let societari = document.getElementById('societari');
-let subvencions = document.getElementById('subvencions');
-let acompanyament = document.getElementById('acompanyament');
-let particulars = document.getElementById('particulars');
+// let laboral = document.getElementById('laboral');
+// let recursos = document.getElementById('recursos');
+// let fiscal = document.getElementById('fiscal');
+// let comptabilitat = document.getElementById('comptabilitat');
+// let societari = document.getElementById('societari');
+// let subvencions = document.getElementById('subvencions');
+// let acompanyament = document.getElementById('acompanyament');
+// let particulars = document.getElementById('particulars');
 
-laboral.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('labo').removeAttribute('hidden');
-    }, 200);
-});
-laboral.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('labo').setAttribute('hidden', 'true');
-    }, 200);
-});
-recursos.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('recurs').removeAttribute('hidden');
-    }, 200);
-});
-recursos.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('recurs').setAttribute('hidden', 'true');
-    }, 200);
-});
-fiscal.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('fisca').removeAttribute('hidden');
-    }, 200);
-});
-fiscal.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('fisca').setAttribute('hidden', 'true');
-    }, 200);
-});
-comptabilitat.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('compta').removeAttribute('hidden');
-    }, 200);
-});
-comptabilitat.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('compta').setAttribute('hidden', 'true');
-    }, 200);
-});
-societari.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('societar').removeAttribute('hidden');
-    }, 200);
-});
-societari.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('societar').setAttribute('hidden', 'true');
-    }, 200);
-});
-subvencions.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('subvencio').removeAttribute('hidden');
-    }, 200);
-});
-subvencions.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('subvencio').setAttribute('hidden', 'true');
-    }, 200);
-});
-acompanyament.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('acompanyamen').removeAttribute('hidden');
-    }, 200);
-});
-acompanyament.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('acompanyamen').setAttribute('hidden', 'true');
-    }, 200);
-});
-particulars.addEventListener('mouseover', function(){
-    setTimeout(function() {
-        document.getElementById('particula').removeAttribute('hidden');
-    }, 200);
-});
-particulars.addEventListener('mouseout', function(){
-    setTimeout(function() {
-        document.getElementById('particula').setAttribute('hidden', 'true');
-    }, 200);
-});
+// laboral.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('labo').removeAttribute('hidden');
+//     }, 200);
+// });
+// laboral.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('labo').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// recursos.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('recurs').removeAttribute('hidden');
+//     }, 200);
+// });
+// recursos.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('recurs').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// fiscal.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('fisca').removeAttribute('hidden');
+//     }, 200);
+// });
+// fiscal.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('fisca').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// comptabilitat.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('compta').removeAttribute('hidden');
+//     }, 200);
+// });
+// comptabilitat.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('compta').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// societari.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('societar').removeAttribute('hidden');
+//     }, 200);
+// });
+// societari.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('societar').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// subvencions.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('subvencio').removeAttribute('hidden');
+//     }, 200);
+// });
+// subvencions.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('subvencio').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// acompanyament.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('acompanyamen').removeAttribute('hidden');
+//     }, 200);
+// });
+// acompanyament.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('acompanyamen').setAttribute('hidden', 'true');
+//     }, 200);
+// });
+// particulars.addEventListener('mouseover', function(){
+//     setTimeout(function() {
+//         document.getElementById('particula').removeAttribute('hidden');
+//     }, 200);
+// });
+// particulars.addEventListener('mouseout', function(){
+//     setTimeout(function() {
+//         document.getElementById('particula').setAttribute('hidden', 'true');
+//     }, 200);
+// });
